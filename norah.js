938bfe4c7c24786dba248364623bef18e534c773
Steven Irby <email@stevenirby.me>
2024-01-10 18:08:37 +0000

    Make it more configurable with the .env and make it easier to quit out of

--- norah.js
+++ norah.js
@@ -1,85 +1,145 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const puppeteer = require('puppeteer');
-const nodemailer = require('nodemailer');
+const puppeteer = require("puppeteer");
+const nodemailer = require("nodemailer");
 
-const URL = 'https://www.twickets.live/catalog/browse';
-// personalise
-const ARTIST = 'Norah Jones'
-const LOCATION = 'London'
-
+// Settings
+const env = process.env;
+const URL = env.TICKET_URL || "https://www.twickets.live/catalog/browse";
+const ARTIST = env.ARTIST || "Norah Jones";
+const LOCATION = env.LOCATION || "London";
+const smtpConfig = {
+  host: env.SMTP_HOST || "smtp.mailgun.org",
+  port: parseInt(env.SMTP_PORT, 10) || 587, // must be integer
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: env.SMTP_USER || "",
+    pass: env.SMTP_PASS || "",
+  },
+};
+const EMAIL_SEND_TYPE = env.EMAIL_SEND_TYPE || "gmail";
+const gmailConfig = {
+  service: "gmail",
+  auth: {
+    user: env.GMAIL_USER || "",
+    pass: env.GMAIL_PASSWORD || "",
+  },
+};
+let emailSent = false;
+let monitoring = true;
 
 async function sendEmail() {
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'archie.cgm@gmail.com',
-            pass: process.env.NODEMAILER_GOOGLE_PASSWORD
+  const transporter = nodemailer.createTransport(EMAIL_SEND_TYPE.toLowerCase() === 'gmail' ? gmailConfig : smtpConfig);
+
+  console.log("Sending email...");
+  try {
+    const info = await transporter.sendMail(
+      {
+        from: env.FROM_EMAIL || "",
+        to: env.TO_EMAIL || "",
+        subject: env.EMAIL_SUBJECT || "Ticket Available",
+        text: env.EMAIL_TEXT || "The ticket you were waiting for is now available!" + ` ${URL}`,
+      },
+      (err, info) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(info);
         }
-    });
-
-    let info = await transporter.sendMail({
-        from: 'archie.cgm@gmail.com',
-        to: 'archie.cgm@gmail.com',
-        subject: 'Ticket Available',
-        text: 'The ticket you were waiting for is now available! https://www.twickets.live/catalog/browse'
-    });
-
-    console.log('Email sent: ' + info.response);
+      }
+    );
+    return info;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-
 async function checkElement(selector, textToMatch, page) {
-  return await page.evaluate((selector, textToMatch) => {
+  return await page.evaluate(
+    (selector, textToMatch) => {
       const element = document.querySelector(selector);
-      console.log(selector, element.textContent)
+      console.log(selector, element.textContent);
       return element && element.textContent.includes(textToMatch);
-  }, selector, textToMatch);
+    },
+    selector,
+    textToMatch
+  );
 }
 
 async function checkTicketAvailability() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
   const page = await browser.newPage();
-  
-  // This will log all console messages from the page
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-
-  await page.goto(URL, { waitUntil: 'networkidle2' });
-
+  const locationSelector = ".multi-select-style input";
   let ticketFound = false;
 
-  for (let i = 0; i < 3; i++) {
-      const artistSelector = `#searchBlockResult_eventName_${i}`;
-      const locationSelector = `#searchBlockResult_venueLocation_${i}`;
+  // This will log all console messages from the page
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-      await page.waitForSelector(artistSelector);
-      await page.waitForSelector(locationSelector);
-      
-      const isCorrectArtist = await checkElement(artistSelector, ARTIST, page);
-      const isCorrectLocation = await checkElement(locationSelector, LOCATION, page);
-      
-      if (isCorrectArtist && isCorrectLocation) {
-          ticketFound = true;
-          break;
-      }
+  await page.goto(URL, { waitUntil: "networkidle2" });
+  await page.waitForSelector(locationSelector);
+  await page.click(locationSelector);
+  await page.keyboard.type(LOCATION);
+  await page.keyboard.press("Enter");
+  // wait 3 seconds for the location to be selected
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  for (let i = 0; i < 3; i++) {
+    const artistSelector = `#searchBlockResult_eventName_${i}`;
+    const locationSelector = `#searchBlockResult_venueLocation_${i}`;
+
+    await page.waitForSelector(artistSelector);
+    await page.waitForSelector(locationSelector);
+
+    const isCorrectArtist = await checkElement(artistSelector, ARTIST, page);
+    const isCorrectLocation = await checkElement(
+      locationSelector,
+      LOCATION,
+      page
+    );
+
+    if (isCorrectArtist && isCorrectLocation) {
+      ticketFound = true;
+      break;
+    }
   }
 
   await browser.close();
   return ticketFound;
 }
 
-
 async function monitor() {
-    while (true) {
-        if (await checkTicketAvailability()) {
-            console.log('Ticket is available!');
-            sendEmail();
-            break;
-        } else {
-            console.log('Ticket is not available yet. Retrying...');
-            await new Promise(resolve => setTimeout(resolve, 10000));
-        }
+  while (monitoring) {
+    const available = await checkTicketAvailability();
+    if (available) {
+      console.log("Ticket is available!");
+      if (!emailSent) {
+        await sendEmail();
+        emailSent = true;
+      }
+    } else {
+      const INTERVAL_IN_SECONDS = Math.floor(
+        Math.random() * (120 - 10 + 1) + 10
+      );
+      console.log(
+        `Ticket is not available yet. Retrying... in ${INTERVAL_IN_SECONDS} seconds`
+      );
+      await new Promise((resolve) =>
+        setTimeout(resolve, INTERVAL_IN_SECONDS * 1000)
+      );
     }
+  }
 }
 
-monitor();
+async function startMonitoring() {
+  await monitor();
+  process.exit(0);
+}
+
+process.on("SIGINT", () => {
+  console.log("Stopping the monitor...");
+  monitoring = false;
+});
+
+startMonitoring();
