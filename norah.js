0f9ccd942f1b9afc7c217bb21fc4f0f0ac38c331
Steven Irby <email@stevenirby.me>
2024-01-16 11:58:53 +0000

    Final changes to the script

--- norah.js
+++ norah.js
@@ -28,7 +28,9 @@ const gmailConfig = {
 let monitoring = true;
 
 async function sendEmail() {
-  const transporter = nodemailer.createTransport(EMAIL_SEND_TYPE.toLowerCase() === 'gmail' ? gmailConfig : smtpConfig);
+  const transporter = nodemailer.createTransport(
+    EMAIL_SEND_TYPE.toLowerCase() === "gmail" ? gmailConfig : smtpConfig
+  );
 
   console.log("Sending email...");
   try {
@@ -37,11 +39,17 @@ async function sendEmail() {
         from: env.EMAIL_FROM || "",
         to: env.EMAIL_TO || "",
         subject: env.EMAIL_SUBJECT || "Ticket Available",
-        text: (env.EMAIL_TEXT || "The ticket you were waiting for is now available! ") + ` ${URL}`,
+        text:
+          (env.EMAIL_TEXT ||
+            "The ticket you were waiting for is now available! ") + ` ${URL}`,
       },
       (err, info) => {
         if (err) {
-          console.error(`Error sending email: ${JSON.stringify(err)}, ${JSON.stringify(info)}`);
+          console.error(
+            `Error sending email: ${JSON.stringify(err)}, ${JSON.stringify(
+              info
+            )}`
+          );
         } else {
           console.log("Email sent!");
           // console.log(info);
@@ -66,46 +74,55 @@ async function checkElement(selector, textToMatch, page) {
 }
 
 async function checkTicketAvailability() {
-  const browser = await puppeteer.launch({
-    headless: "new",
-  });
-  const page = await browser.newPage();
-  const locationSelector = ".multi-select-style input";
-  let ticketFound = false;
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
+    const locationSelector = ".multi-select-style input";
+    let ticketFound = false;
 
-  // This will log all console messages from the page
-  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+    // This will log all console messages from the page
+    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-  await page.goto(URL, { waitUntil: "networkidle2" });
-  await page.waitForSelector(locationSelector);
-  await page.click(locationSelector);
-  await page.keyboard.type(LOCATION);
-  await page.keyboard.press("Enter");
-  // wait 3 seconds for the location to be selected
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-
-  for (let i = 0; i < 3; i++) {
-    const artistSelector = `#searchBlockResult_eventName_${i}`;
-    const locationSelector = `#searchBlockResult_venueLocation_${i}`;
-
-    await page.waitForSelector(artistSelector);
+    await page.goto(URL, { waitUntil: "networkidle2" });
     await page.waitForSelector(locationSelector);
+    await page.click(locationSelector);
+    await page.keyboard.type(LOCATION);
+    await page.keyboard.press("Enter");
+    // wait 3 seconds for the location to be selected
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const isCorrectArtist = await checkElement(artistSelector, ARTIST, page);
-    const isCorrectLocation = await checkElement(
-      locationSelector,
-      LOCATION,
-      page
-    );
+    for (let i = 0; i < 3; i++) {
+      const artistSelector = `#searchBlockResult_eventName_${i}`;
+      const locationSelector = `#searchBlockResult_venueLocation_${i}`;
 
-    if (isCorrectArtist && isCorrectLocation) {
-      ticketFound = true;
-      break;
+      await page.waitForSelector(artistSelector);
+      await page.waitForSelector(locationSelector);
+
+      const isCorrectArtist = await checkElement(artistSelector, ARTIST, page);
+      const isCorrectLocation = await checkElement(
+        locationSelector,
+        LOCATION,
+        page
+      );
+
+      if (isCorrectArtist && isCorrectLocation) {
+        ticketFound = true;
+        break;
+      }
+    }
+
+    return ticketFound;
+  } catch (e) {
+    console.error(e);
+    return false;
+  } finally {
+    if (browser) {
+      await browser.close();
     }
   }
-
-  await browser.close();
-  return ticketFound;
 }
 
 async function monitor() {
